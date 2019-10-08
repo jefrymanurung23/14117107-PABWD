6fd8ecd220e8d901231d291769a2071aa2ceabb1
Jefri Manurung <jefrymanurung23@gmail.com>
2019-10-08 14:14:07 +0700

    Upload latihan 5.php

---
+++ Pertemuan 6/Latihan Pertama/Latihan 5.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contoh Skrip PHP</title>
+</head>
+<body>
+    <?php
+        $i = 2;
+        do {
+            echo "\$i = $i <br>";
+            $i++;
+        } while ($i < 5);
+    ?>
+</body>
+</html>
