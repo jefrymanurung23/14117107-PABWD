5cded211409916dde71690244afbfd6aabf2459d
Jefri Manurung <jefrymanurung23@gmail.com>
2019-10-08 14:15:48 +0700

    Upload latihan 6.php

---
+++ Pertemuan 6/Latihan Pertama/Latihan 6.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contoh Skrip PHP</title>
+</head>
+<body>
+    <?php
+        for ($i=0; $i <= 10; $i++) { 
+            if ($i == 6) {
+                break;
+            }
+            echo "\$i = $i <br>";
+        }
+    ?>
+</body>
+</html>
