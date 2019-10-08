d3c30654030f67562de4e1940484079dc08209ba
Jefri Manurung <jefrymanurung23@gmail.com>
2019-10-08 14:01:36 +0700

    Uplaod latihan 2.php

---
+++ Pertemuan 6/Latihan 2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contoh Skrip PHP</title>
+</head>
+<body>
+    <?php
+        $a = 3;
+        $b = 7;
+        $a += 5;
+        $b = ($c = 11) + 3;
+        echo "Nilai variable a = $a <br>";
+        echo "Nilai variable b = $b <br>";
+        echo "Nilai variable c = $c <br>";
+    ?>
+</body>
+</html>
