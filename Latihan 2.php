598f91a2de0c30af65a395a3b71c6239eafa14ad
Jefri Manurung <jefrymanurung23@gmail.com>
2019-10-22 14:21:12 +0700

    upload latihan 2.php

---
+++ Latihan 2.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Latihan 2</title>
+</head>
+
+<body>
+    <div class="container">
+        <form method="GET" action="Latihan 2 - Hasil.php">
+            <div class="form-group">
+                <label>Nama</label>
+                <input type="text" class="form-control" name="nama" placeholder="Nama Lengkap">
+            </div>
+            <div class="form-group">
+                <label>Warna</label>
+                <select multiple class="form-control" id="exampleFormControlSelect2">
+                    <option value="merah" disabled selected hidden>-- Pilih --</option>
+                    <option value="merah">Merah</option>
+                    <option value="kuning">Kuning</option>
+                    <option value="hijau">Hijau</option>
+                    <option value="putih">Putih</option>
+                    <option value="hitam">Hitam</option>
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary" name="show">Submit</button>
+        </form>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
