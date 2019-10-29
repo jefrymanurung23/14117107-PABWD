b046c57ce7ca2c82212c0bec2d0fe3240c044957
Jefri Manurung <jefrymanurung23@gmail.com>
2019-10-29 13:58:52 +0700

    update percobaan 2 d.php

--- Pertemuan 8/Percobaan 2 D.php
+++ Pertemuan 8/Percobaan 2 D.php
@@ -7,7 +7,7 @@
 <BODY>
     <h1>Koneksi database dengan mysql_fetch_assoc</h1>
     <?php
-        $conn = mysqli_connect ("localhost","root","", "percobaan8_jefri") or die ("koneksi gagal");
+        $conn = mysqli_connect ("localhost","root","", "praktikum8_jefri") or die ("koneksi gagal");
         $hasil = mysqli_query($conn, "SELECT * FROM liga");
         
         while ($row = mysql_fetch_row($hasil)) {
