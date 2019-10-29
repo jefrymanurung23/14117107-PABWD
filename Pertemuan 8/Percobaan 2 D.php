4901ee4b4686e8e59ecdbb0b660c97e294c3b483
Jefri Manurung <jefrymanurung23@gmail.com>
2019-10-29 13:56:55 +0700

    upload percobaan 2 d.php

---
+++ Pertemuan 8/Percobaan 2 D.php
@@ -0,0 +1,21 @@
+<HTML>
+
+<HEAD>
+    <title>Koneksi Database MySQL</title>
+</HEAD>
+
+<BODY>
+    <h1>Koneksi database dengan mysql_fetch_assoc</h1>
+    <?php
+        $conn = mysqli_connect ("localhost","root","", "percobaan8_jefri") or die ("koneksi gagal");
+        $hasil = mysqli_query($conn, "SELECT * FROM liga");
+        
+        while ($row = mysql_fetch_row($hasil)) {
+            echo "Liga " .$row[1];
+            echo " mempunyai " .$row[2];
+            echo " wakil di liga champion <br>";
+        }
+    ?>
+</BODY>
+
+</HTML>
