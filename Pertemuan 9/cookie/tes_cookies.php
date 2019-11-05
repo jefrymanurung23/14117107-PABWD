ffa21e9faf387eebda8e1dbcbe4f52ae0c0c3b1f
Jefri Manurung <jefrymanurung23@gmail.com>
2019-11-05 14:11:56 +0700

    update tes_cookies.php + analisis

--- Pertemuan 9/cookie/tes_cookies.php
+++ Pertemuan 9/cookie/tes_cookies.php
@@ -3,10 +3,12 @@
 <html>
 <body>
     <?php if(count($_COOKIE) > 0) {
-    echo "Cookies enabled.";
+        echo "Cookies enabled.";
     } else {
-    echo "Cookies disabled.";
+        echo "Cookies disabled.";
     } ?>
+
+    pada tampilan browser biasa, maka akan masuk kedalam cookies enabled, karena dapat diketahui time() atau waktu yang sekarang dalam detik. Sedangkan pada browser incignito, maka akan masuk kedalam cookies disabled, karena time() tidak diketahui, sehingga nilainya kosong.
 
 </body>
 </html>
